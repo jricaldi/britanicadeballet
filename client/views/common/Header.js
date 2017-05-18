63f3b0a23d28456eaf4c2c63d5fa5b85e5fe27c9
Jorge Ricaldi <jorge.ricaldi@gmail.com>
2017-05-17 20:07:15 -0500

    FEAT: smooth scrolling implementation

--- client/views/common/Header.js
+++ client/views/common/Header.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import Scroll from 'react-scroll';
 import './header.scss';
+import logo from '../../../static/images/logo.png';
+
+const Link = Scroll.Link;
 
 const Header = () =>
   <header>
-    <a href="/" id="logo"><img src="images/logo.png" alt="Britanica de ballet" /></a>
+    <a href="/" id="logo"><img src={ logo } alt="Britanica de ballet" /></a>
     <nav>
       <ul>
-        <li><a href="#quienes-somos">Quienes Somos</a></li>
-        <li><a href="#clases">Clases</a></li>
-        <li><a href="#profesoras">Profesoras</a></li>
-        <li><a href="#horarios">Horarios</a></li>
-        <li><a href="#contacto">Contactos</a></li>
+        <li><Link href="#quienes-somos">Quienes Somos</Link></li>
+        <li><Link href="#clases">Clases</Link></li>
+        <li><Link href="#profesoras">Profesoras</Link></li>
+        <li><Link href="#horarios">Horarios</Link></li>
+        <li><Link href="#contacto">Contactos</Link></li>
       </ul>
     </nav>
   </header>;
