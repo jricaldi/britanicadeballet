4c359db90e70ba6b03f25fa0da8f65e9c20913f1
Jorge Ricaldi <jorge.ricaldi@gmail.com>
2017-06-06 22:16:59 -0500

    FEAT: Using a new layout for react

--- client/views/common/Header.js
+++ client/views/common/Header.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import Headroom from 'headroom.js';
 import $ from 'jquery';
 import './header.scss';
+import { OnMobile, OnAtLeastTablet } from '../../layout/layoutGrid';
 import logo from '../../../static/images/logo.png';
 import '../../../static/libs/hamburger/styles.scss';
 
@@ -23,33 +24,40 @@ export default class Header extends Component {
     $('.bottom-menu').toggleClass('bottom-animate');
   }
 
-  render = () => <header ref={ node => { this.header = node; } }>
+  render = () => <header>
     <div class="wrapper">
-      <Link to="/" class="logo"><img src={ logo } alt="Britanica de ballet" /></Link>
-      <nav class="nav-large">
-        <ul>
-          <li><Slink to="id-clases" smooth="easeInOutCubic" class="underline">Clases</Slink></li>
-          <li><Slink to="id-profesoras" smooth="easeInOutCubic" class="underline">Profesoras</Slink></li>
-          <li><Slink to="id-horarios" smooth="easeInOutCubic" class="underline">Horarios</Slink></li>
-          <li><Slink to="id-contacto" smooth="easeInOutCubic" class="underline">Contactos</Slink></li>
-        </ul>
-      </nav>
-      <div class="nav-small">
-        <ul class="main-nav">
-          <li><a href="">Home</a></li>
-          <li><a href="">About</a></li>
-          <li><a href="">Our Work</a></li>
-          <li><a href="">Products</a></li>
-          <li><a href="">Contact Us</a></li>
-        </ul>
-      </div>
-      <a class="nav-trigger" role="button" onClick={ this.showMobileNav } >
-        <div class="hamburger">
-          <div class="menui top-menu" />
-          <div class="menui mid-menu" />
-          <div class="menui bottom-menu" />
+      <OnAtLeastTablet ref={ node => { this.header = node; } }>
+        <Link to="/" class="logo"><img src={ logo } alt="Britanica de ballet" /></Link>
+        <nav class="nav-large">
+          <ul>
+            <li><Slink to="id-clases" smooth="easeInOutCubic" class="underline">Clases</Slink></li>
+            <li><Slink to="id-profesoras" smooth="easeInOutCubic" class="underline">Profesoras</Slink></li>
+            <li><Slink to="id-horarios" smooth="easeInOutCubic" class="underline">Horarios</Slink></li>
+            <li><Slink to="id-contacto" smooth="easeInOutCubic" class="underline">Contactos</Slink></li>
+          </ul>
+        </nav>
+      </OnAtLeastTablet>
+      <OnMobile>
+        <Link to="/" class="logo"><img src={ logo } alt="Britanica de ballet" /></Link>
+        <div>
+          <nav role="navigation">
+            <ul class="main-nav">
+              <li><a href="">Home</a></li>
+              <li><a href="">About</a></li>
+              <li><a href="">Our Work</a></li>
+              <li><a href="">Products</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </nav>
         </div>
-      </a>
+        <a class="nav-trigger" role="button" onClick={ this.showMobileNav } >
+          <div class="hamburger">
+            <div class="menui top-menu" />
+            <div class="menui mid-menu" />
+            <div class="menui bottom-menu" />
+          </div>
+        </a>
+      </OnMobile>
     </div>
   </header>;
 }
