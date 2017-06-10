3aa1dcd164c005956bfe6f69c144b97eaa243394
jricaldi <jorge.ricaldi@gmail.com>
2017-06-10 16:10:35 -0500

    FIX: changes for home component

--- client/containers/QuienesSomos/QuienesSomos.js
+++ client/containers/QuienesSomos/QuienesSomos.js
@@ -3,8 +3,8 @@ import './quienesSomos.scss';
 
 const QuienesSomos = () =>
   <section class="about">
-    <h2 class="about__main-text">Expresa el arte que llevas adentro a través de tu cuerpo</h2>
-    <h2 class="about__secondary-text">Más de <span class="number">50</span> años de experiencia</h2>
+    <h2 class="about__text">Expresa el arte que llevas adentro a través de tu cuerpo</h2>
+    <h2 class="about__text about__text--secondary">Más de <span class="number">50</span> años de experiencia</h2>
   </section>;
 
 export default QuienesSomos;
