c9a4cfeab1b7373a568740cd2574c7ee5844b747
Jorge Ricaldi <jorge.ricaldi@gmail.com>
2017-06-13 17:47:01 -0500

    FEAT: add new teacher to the database

--- client/helpers/database.js
+++ client/helpers/database.js
@@ -1,15 +1,30 @@
 import profesoraDiana from '../../static/images/profesora-diana.jpg';
+import profesoraMili from '../../static/images/profesora-mili.jpg';
+import profesoraMargarita from '../../static/images/profesora-margarita.jpg';
 
 export const dataProfesoras = [
   {
     nombre: 'Diana',
     apellidoPaterno: 'Untiveros',
-    apellidoMaterno: 'Villa',
-    detalle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor \
-    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
-    ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in \
-    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non \
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    apellidoMaterno: '',
+    detalle: 'Inicia sus estudios de ballet clásico a temprana edad en nuestra Academia con diferentes maestras, entre ellas Rosina Müller. Paralelamente lleva cursos de música. En enero del 2014 culmina sus estudios de Musicología e Italiano en Alemania. Además de enseñar ballet y tap, es profesora particular de piano, canto e idiomas en la Academia Británica de Ballet.',
     foto: profesoraDiana,
+  },
+  {
+    nombre: 'Mili',
+    apellidoPaterno: 'Mili Nugent',
+    apellidoMaterno: '',
+    detalle: 'Se dedica a la enseñanza del ballet clásico y tap desde 1991 en nuestra academia. Inició sus estudios de ballet en la Academia de Ballet Miraflores.Culminó sus estudios en la Academia Británica de Ballet bajo la dirección de la Sra. Rosina Müller. \
+    Aprende el método de la Royal Academy of Dance de Londres.En el año 1988 se incorpora a la Cía. Danza Viva donde permanece hasta el año 1991. Actualmente es parte del staff de profesores de la Escuela Independiente Danza Viva y dicta clases en diversos nidos y colegios.',
+    foto: profesoraMili,
+  },
+  {
+    nombre: 'Margarita',
+    apellidoPaterno: 'Untiveros',
+    apellidoMaterno: '',
+    detalle: 'Inicia sus estudios de danza clásica a temprana edad en la Asociación Choriartium en Lima con la maestra Lucy Telge y paralelamente lleva cursos de música con Teresa Costa. \
+    En Caracas forma parte del grupo de danza de la Academia Interamericana de Ballet con el maestro Mario Ignisci y continúa sus estudios musicales en el conservatorio Juan José Landaeta de Caracas, tomando clases de perfeccionamiento de piano con la destacada pianista norteamericana Harriet Seher. \
+    Desde 1995 es Directora de la Academia Británica de Ballet, diplomada como profesora de la Royal Academy of Dance de Inglaterra. Debido a su trayectoria musical y su afinidad por diversos idiomas ha implementado desde el año 2014 clases individuales de piano, violín y canto así como clases de alemán, ingles e italiano. Esta expansión extra-curricular permite a nuestras alumnas formarse en dichos rubros y tomar clases individuales antes o después de sus clases de ballet en nuestra academia.',
+    foto: profesoraMargarita,
   },
 ];
