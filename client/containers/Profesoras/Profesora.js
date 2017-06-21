68131374f0bef67c960ce6f79d6079171a9a547b
jricaldi <jorge.ricaldi@gmail.com>
2017-06-21 18:24:30 -0500

    FEAT: new effects for images

--- client/containers/Profesoras/Profesora.js
+++ client/containers/Profesoras/Profesora.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import aos from 'aos';
+import 'aos/dist/aos.css';
+
 import './profesora.scss';
 
-const Profesora = ({ info }) =>
-  <article class="profesora row">
-    <div class="profesora__image col-xs-12 center-xs">
-      <img src={ info.foto } alt={ info.nombre } />
-    </div>
-    <div class="profesora__info col-xs-12">
-      <span class="profesora__info__name">{ info.nombre }</span>
-      <span class="profesora__info__surname">{ info.apellidoPaterno }</span>
-    </div>
-    <div class="profesora__details col-xs-12">
-      { info.detalle }
-    </div>
-  </article>;
+export default class Profesora extends Component {
 
-export default Profesora;
+  componentDidMount = () => {
+    aos.init();
+  }
+
+  render = () => {
+    const { info } = this.props;
+    return <article class="profesora row">
+      <div class="profesora__image col-xs-12 center-xs">
+        <img src={ info.foto } alt={ info.nombre } data-aos="zoom-in" data-aos-once />
+      </div>
+      <div class="profesora__info col-xs-12">
+        <span class="profesora__info__name" data-aos="zoom-out-right">{ info.nombre }</span>
+        <span class="profesora__info__surname" data-aos="zoom-out-right">{ info.apellidoPaterno }</span>
+      </div>
+      <div class="profesora__details col-xs-12">
+        { info.detalle }
+      </div>
+    </article>;
+  }
+}
