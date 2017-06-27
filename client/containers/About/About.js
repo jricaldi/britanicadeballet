06d3f1540b222534712c7d423d6c19d20408e85e
jricaldi <jorge.ricaldi@gmail.com>
2017-06-27 18:46:37 -0500

    FEAT: contact section 1.0

--- client/containers/About/About.js
+++ client/containers/About/About.js
@@ -4,8 +4,8 @@ import './about.scss';
 const About = () =>
   <section class="about">
     <div class="row">
-      <h2 class="about__text col-xs-12">Expresa el arte que llevas adentro a través de tu cuerpo</h2>
-      <h2 class="about__text about__text--secondary col-xs-12">Más de <span class="number">50</span> años de experiencia</h2>
+      <h2 class="about__text col-xs-12">Expresa el arte que hay en ti</h2>
+      <h2 class="about__text about__text--secondary col-xs-12">Más de <span class="number">61</span> años de experiencia</h2>
     </div>
   </section>;
 
--- client/containers/Contacto/Contacto.js
+++ client/containers/Contacto/Contacto.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './contacto.scss';
 
-const Contacto = () =>
-  <section class="contacto" id="id-contacto">
-    <div class="contacto__box">
-      <div class="contacto__box__button">
-        <a>facebook</a>
-        <a>youtube</a>
-        <a>twitter</a>
+export default class Contacto extends Component {
+  render = () => {
+    return <section class="contacto" id="id-contacto">
+      <div class="contacto__box">
+        <div class="contacto__box__button">
+          <a>facebook</a>
+          <a>youtube</a>
+          <a>twitter</a>
+        </div>
+        <div class="contacto__box__info">
+          <div class="contacto__box__info__address">
+            <a href="tel:+51014481113">+51 (01) 4481113</a>
+            <p>
+              Avenida Alfredo Franco 200, Urbanización Chama, Surco
+            </p>
+          </div>
+          <div class="contacto__box__info__map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.7176653663755!2d-77.00035304905748!3d-12.131459991368802!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105b8087d4a59a9%3A0x4a3ecbdc4a007ceb!2sAcademia+Brit%C3%A1nica+de+Ballet!5e0!3m2!1ses-419!2sus!4v1498017371971" allowFullScreen />
+          </div>
+        </div>
       </div>
-      <div class="contacto__box__info">
-      hello word
-      </div>
-      <div class="contacto__box__map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.7176653663755!2d-77.00035304905748!3d-12.131459991368802!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105b8087d4a59a9%3A0x4a3ecbdc4a007ceb!2sAcademia+Brit%C3%A1nica+de+Ballet!5e0!3m2!1ses-419!2sus!4v1498017371971" allowFullScreen />
-      </div>
-    </div>
-  </section>;
-
-export default Contacto;
+    </section>;
+  }
+}
